9efb4a024044ab269a6e571fb52ab5dd04d01a4b
Peter Lewis <peter@werl.me>
2019-02-04 19:17:10 -0700

    add package-info.java's

---
+++ src/main/java/me/werl/miscaesthetics/misc/package-info.java
@@ -0,0 +1,7 @@
+@ParametersAreNonnullByDefault
+@MethodsReturnNonnullByDefault
+package me.werl.miscaesthetics.misc;
+
+import mcp.MethodsReturnNonnullByDefault;
+
+import javax.annotation.ParametersAreNonnullByDefault;
---
+++ src/main/java/me/werl/miscaesthetics/package-info.java
@@ -0,0 +1,7 @@
+@ParametersAreNonnullByDefault
+@MethodsReturnNonnullByDefault
+package me.werl.miscaesthetics;
+
+import mcp.MethodsReturnNonnullByDefault;
+
+import javax.annotation.ParametersAreNonnullByDefault;
